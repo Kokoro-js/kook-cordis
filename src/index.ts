ab8c659f16c4f85dffe87c74dba774aea8da52e8
ahdg <36436808+ahdg6@users.noreply.github.com>
2024-03-24 12:05:50 +0800

    正确导出 enmu

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './Logger';
 export * from './card';
 export * as utils from './utils';
 export type * as Kook from './types';
+export { MessageType, Permissions, UserStatus } from './types';
 export { CommandInstance } from './services/commander/commander';
 
 export { default as axios } from 'axios';
