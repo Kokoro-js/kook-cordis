1c9bf131afffb2af80ca6bdbed6a222649052a7f
MODcraft <admin@ncgs.top>
2024-10-27 11:57:09 +0800

    Export api

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,7 @@ export * from './bot';
 export * from './Logger';
 export * from './card';
 export * as utils from './utils';
+export * as api from './api';
 export type * as Kook from './types';
 export { MessageType, Permissions, UserStatus } from './types';
 export { CommandInstance } from './services/commander/commander';
